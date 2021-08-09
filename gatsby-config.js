a2386d01fccce7ce8e04df01f9e0ab9aa9f34652
Gregg Shanefelt <gshanefelt@phase2technology.com>
2021-08-09 17:30:48 -0400

    initial comit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,44 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "jsonAPI",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'jsonAPI',
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-transformer-remark",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-transformer-remark',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
+    },
+    {
+      resolve: 'gatsby-source-custom-api',
+      options: {
+        url: 'https://8080-tan-crane-l9qtnzal.ws-us13.gitpod.io/en/jsonapi/node/recipe',
+        rootKey: 'data',
+        schemas: {},
+      },
     },
   ],
 };
